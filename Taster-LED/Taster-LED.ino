f7522d38dcb11e76ecf644b67fa1c7815697379c
June Nana <juniouethy@gmail.com>
2026-06-21 21:51:25 +0200

    Taster-LED.ino

--- Taster-LED/Taster-LED.ino
+++ Taster-LED/Taster-LED.ino
@@ -2,16 +2,16 @@ int tasterPin = 2;   // Taster an Pin 2
 int ledPin = 13;     // LED an Pin 13
 
 void setup() {
-  pinMode(ledpin, OUTPUT);
-  pinMode(tasterpin, INPUT_PULLUP);  // Pin 2 als Eingang
+  pinMode(ledPin, OUTPUT);
+  pinMode(tasterPin, INPUT_PULLUP);  // Pin 2 als Eingang
 }
 
 void loop() {
   int zustand = digitalRead(2); // Taster lesen
 
   if (zustand == LOW) {       
-    digitalWrite(ledpin, HIGH); // LED an
+    digitalWrite(ledPin, HIGH); // LED an
   } else {
-    digitalWrite(ledpin, LOW);  // LED aus
+    digitalWrite(ledPin, LOW);  // LED aus
   }
 }
